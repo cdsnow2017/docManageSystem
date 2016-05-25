35b0ff884e23a35afaabba52ef2ad7064a90b573
cdsnow2017 <cdsnow2017@gmail.com>
2016-05-26 00:14:45 +0800

    后台框架搭建

---
+++ src/main/java/edu/ynu/docmanagesystem/controller/UserController.java
@@ -0,0 +1,5 @@
+package edu.ynu.docmanagesystem.controller;
+
+public class UserController {
+
+}
---
+++ src/main/java/edu/ynu/docmanagesystem/service/UserService.java
@@ -0,0 +1,29 @@
+package edu.ynu.docmanagesystem.service;
+
+/**
+ * description:
+ * @author cdsnow2017@163.com
+ * @since 2016年5月22日 下午12:38:52
+ * @version 1.0
+ */
+interface UserService {
+
+	/**
+	 * description:登录验证
+	 * @param userId 用户id
+	 * @param roleId 选择的登录角色id
+	 * @return -1：用户不存在  1：登录成功  0：用户无此角色权限 -2：密码错误
+	 */
+	int loginVerify(Integer userId, Integer roleId); 
+	
+	/**
+	 * description: 验证用户是否具有对请求的用户功能的权限
+	 * @param userId 用户id
+	 * @param resourceId 资源id
+	 * @param userAuthorityId 所请求的功能id
+	 * @return 0：不具有 1：具有 
+	 */
+	int userAuthenticate(Integer userId,Integer resourceId,Integer userAuthorityId); 
+	
+	
+}
