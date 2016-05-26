f5e024d229131f28f7b3ce59b801feedfb352846
cdsnow2017 <cdsnow2017@gmail.com>
2016-05-27 01:22:39 +0800

    后台登陆验证，前后台整合

--- src/main/java/edu/ynu/docmanagesystem/controller/UserController.java
+++ src/main/java/edu/ynu/docmanagesystem/controller/UserController.java
@@ -2,10 +2,13 @@ package edu.ynu.docmanagesystem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import edu.ynu.docmanagesystem.po.User;
 import edu.ynu.docmanagesystem.poExtend.State;
+import edu.ynu.docmanagesystem.poExtend.UserExtend;
 import edu.ynu.docmanagesystem.service.UserService;
 import edu.ynu.docmanagesystem.util.BeanFactory;
 
@@ -16,8 +19,8 @@ public class UserController {
 	private UserService userService;
 
 	@RequestMapping("/login")
-	public @ResponseBody State login(Integer userId, String password, Integer roleId) throws Exception {
-		return BeanFactory.getState(userService.loginVerify(userId, password, roleId) + "");
+	public @ResponseBody State login(@RequestBody UserExtend user) throws Exception {
+		return BeanFactory.getState(userService.loginVerify(user.getUserid(), user.getPassword(), user.getRoleid()) + "");
 
 	}
 
